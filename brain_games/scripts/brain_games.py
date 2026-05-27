be221ad849840ab83bcb788582ad0876cf258960
oleg pogorelov <opogorelov@rbc.ru>
2026-05-27 19:26:14 +0300

    fix failure of pipeline for in brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,11 +3,5 @@ from brain_games.cli import welcome_user
 def main():
     welcome_user()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
-
-def list_of_text(x):
-    return x
-
-print(sorted(list_of_text()))
